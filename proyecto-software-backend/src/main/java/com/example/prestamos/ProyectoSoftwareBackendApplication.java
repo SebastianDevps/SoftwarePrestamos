4c990677f6b4ba1f895e83b22e41960a0f669ae8
dualbert29 <dualber29@gmail.com>
2024-05-08 17:55:53 -0500

    implemantando los proyectos para el backend y el frontend

---
+++ proyecto-software-backend/src/main/java/com/example/prestamos/ProyectoSoftwareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.prestamos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSoftwareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSoftwareBackendApplication.class, args);
+	}
+
+}
