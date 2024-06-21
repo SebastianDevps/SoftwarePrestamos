768277de2f59d0c238d7b24512a582ee263399a3
Sebastian Guerra <149134186+SebastianDevps@users.noreply.github.com>
2024-06-20 23:16:33 -0500

    Update

--- software-Prestamos-frontend/client/src/components/form_cliente/FormularioCliente.jsx
+++ software-Prestamos-frontend/client/src/components/form_cliente/FormularioCliente.jsx
@@ -151,11 +151,11 @@ const FormularioCliente = ({ onClick, cliente }) => {
             <label className="block text-sm font-medium text-gray-700">Correo</label>
             <input
               type="email"
-              className={`mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm ${
-                errors.correo ? 'border-red-500' : ''
+              className={`mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm
+                ${ errors.correo ? 'border-red-500' : ''
               }`}
               placeholder="Ingrese su correo"
-              {...register('correo', { required: 'Este campo es requerido', pattern: /^\S+@\S+$/i })}
+              {...register('correo', {pattern: /^\S+@\S+$/i })}
             />
             {errors.correo && <span className="text-red-500 text-sm">{errors.correo.message}</span>}
           </div>
